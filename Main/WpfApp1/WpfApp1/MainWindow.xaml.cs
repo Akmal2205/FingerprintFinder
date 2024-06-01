0fd91dc785045ecb594813449833c946f0a40e4b
Akmal2205 <syarafiakml2004@gmail.com>
2024-06-01 17:01:21 +0700

    ft : GUI

---
+++ Main/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    ///
+
+    public partial class MainWindow : Window
+    {
+        public string Method { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseLeftButton(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                Console.WriteLine("Mouse Left Button Down on windowGrid");
+                DragMove();
+            }
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnOpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png;*.jpg;*.jpeg;*.bmp;*.gif)|*.png;*.jpg;*.jpeg;*.bmp;*.gif";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string filePath = openFileDialog.FileName;
+                try
+                {
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(filePath);
+                    bitmap.EndInit();
+
+                    Image image = new Image();
+                    image.Source = bitmap;
+
+                    imageContainer.Child = image;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image: {ex.Message}");
+                }
+            }
+        }
+
+        private void btnBM_Click(object sender, RoutedEventArgs e)
+        {
+            this.Method = "BM";
+            Console.WriteLine(this.Method);
+        }
+
+        private void btnKMP_Click(object sender, RoutedEventArgs e)
+        {
+            this.Method = "KMP";
+            Console.WriteLine(this.Method);
+        }
+    }
+}
